1accbe5971d704686cc391a6c3984aecbbe88241
Seraphim132 <sofoworaemmanuel0@gmail.com>
2026-04-28 22:00:13 +0100

    more adjustment made

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Loading from './Loading'
-import Button from './button'
+import Button from './Button'
 import axios from 'axios'
 import { FaUserLock } from 'react-icons/fa'
 
